f087ff338601862f2ee6e6097a384d0d48c543a0
morganDHMurray <morgan.murray12@hotmail.com>
2020-01-10 15:36:09 +0800

    learning biz

---
+++ src/components/bear.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import BearSVG from '../images/bear.svg';
+
+const Bear = () => {
+  return <img src={BearSVG} alt="red bear of dreams" className="bearimg" />;
+};
+
+export default Bear;

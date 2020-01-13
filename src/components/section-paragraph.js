4015e347ac9587cc9dc9e21cd2e517a3508cdc87
morganDHMurray <morgan.murray12@hotmail.com>
2020-01-13 12:53:23 +0800

    Updates

--- src/components/section-paragraph.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SectionParagraph = ({ children }) => (
-  <p className="section-paragraph">{children}</p>
-);
-
-export default SectionParagraph;
--- src/components/section-title.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SectionTitle = ({ children }) => (
-  <p className="section-title">{children}</p>
-);
-
-export default SectionTitle;

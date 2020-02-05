9e68b4c1dabb57035d68202cbb9cc1fa66aabeb0
morganDHMurray <morgan.murray12@hotmail.com>
2020-02-05 14:18:33 +0800

    updated icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#D94C47`,
         display: `standalone`,
-        icon: `src/images/bear.svg`, // This path is relative to the root of the site.
+        icon: `src/images/icon.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

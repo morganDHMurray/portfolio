c3ef5f9dbfbf80e2374744ef6b7d3237afc4c19d
morganDHMurray <morgan.murray12@hotmail.com>
2020-04-06 16:39:27 +0800

    Went bezerk on styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import SectionTitle from './section-title';
 import SectionParagraph from './section-paragraph';
+import PrimaryButton from './primary-button.js';
+import SecondaryButton from './secondary-button';
+import TextField from './text-field';
 
 const Contact = () => {
   return (
     <div className="contact-container">
       <SectionTitle>Contact</SectionTitle>
       <SectionParagraph> Hey please get in touch</SectionParagraph>
-      <div className="form-container">
-        <form method="post" action="#">
-          <label>
-            Name
-            <input type="text" name="name" id="name" />
-          </label>
-          <label>
-            Email
-            <input type="email" name="email" id="email" />
-          </label>
-          <label>
-            Subject
-            <input type="text" name="subject" id="subject" />
-          </label>
-          <label>
-            Message
-            <textarea name="message" id="message" rows="5" />
-          </label>
-          <button type="submit">Send</button>
-          <input type="reset" value="Clear" />
-        </form>
-      </div>
+      <form className="form-container" method="post" action="#">
+        <TextField>
+          Name
+          <input type="text" name="name" id="name" />
+        </TextField>
+        <TextField>
+          Email
+          <input type="email" name="email" id="email" />
+        </TextField>
+        <TextField>
+          Subject
+          <input type="text" name="subject" id="subject" />
+        </TextField>
+        <TextField>
+          Message
+          <textarea name="message" id="message" rows="5" />
+        </TextField>
+        <div className="form-buttons">
+          <PrimaryButton>Submit</PrimaryButton>
+          <SecondaryButton>Clear</SecondaryButton>
+        </div>
+      </form>
     </div>
   );
 };

cdfe39f397ec618709cd2cc321d575cb6befb478
morganDHMurray <morgan.murray12@hotmail.com>
2020-04-20 10:24:20 +0800

    adding switches + package updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,22 +10,24 @@ import PropTypes from 'prop-types';
 import '../css/layout.css';
 import '../css/typography.css';
 import Scrollspy from 'react-scrollspy';
+import RedSwitch from './red-switch';
+import BlueSwitch from './blue-switch';
 // ..
 
 const Layout = ({ children }) => {
   return (
     <div className="layout-grid">
-      {/*<div>
-        <svg height="1000" width="22" className="svg-line">
-          <line x1="20" y1="30" x2="20" y2="950" />
-        </svg>
-      </div>*/}
+      <div className="switch-container">
+        <RedSwitch></RedSwitch>
+        <BlueSwitch></BlueSwitch>
+      </div>
       <nav className="nav">
         <ul>
           <Scrollspy
             items={['about', 'skills', 'projects', 'contact']}
+            className="nav-flex"
             currentClassName="is-current"
-            offset="50"
+            offset="0"
           >
             <li>
               <a href="#about">
